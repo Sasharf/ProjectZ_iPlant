d2c17b58d1f8a5b87990a0472648121271d25fb9
Sneguro4ka <yvova88@yahoo.com>
2018-01-10 18:57:21 +0200

    added get_real_SENSORDATA from the adc. requirements - install Adafruit-ADS1x15

--- Hardware/Heat.py
+++ Hardware/Heat.py
@@ -1,14 +1,18 @@
-
 #import RPi.GPIO as GPIO
 from random import randint
+import Adafruit_ADS1x15
 #GPIO.setmode(GPIO.BCM)
 
 
 class Heat:
     pin_num = None
+    adc = Adafruit_ADS1x15.ADS1015()
 
     def __init__(self, pin):
         self.pin_num = pin
+
+    def get_real_status(self):
+        return self.adc.read_adc(self.pin_num, gain=1)
 
     def get_status(self):
         return randint(0, 1000)
--- Hardware/WaterLvl.py
+++ Hardware/WaterLvl.py
@@ -1,16 +1,21 @@
 #import RPi.GPIO as GPIO
 from random import randint
+import Adafruit_ADS1x15
 #GPIO.setmode(GPIO.BCM)
 
 
 class WaterLvl:
     pin_num = None
+    adc = Adafruit_ADS1x15.ADS1015()
 
     def __init__(self, pin):
         self.pin_num = pin
 
     def get_status(self):
         return randint(0, 1000)
+
+    def get_real_water_lvl(self):
+        return self.adc.read_adc(self.pin_num, gain=1)
 
     def get_water_lvl(selfs):
         return randint(0, 1000)
