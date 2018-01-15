feac3c3c9e5b2bc0ef5007769b36dcb1863b323f
sasha <sasharf@gmail.com>
2018-01-15 16:14:32 +0200

    added program template

--- Hardware/Moist.py
+++ Hardware/Moist.py
@@ -6,13 +6,19 @@ import Adafruit_ADS1x15
 
 class Moist:
     pin_num = None
-    adc = Adafruit_ADS1x15.ADS1015()
+    try:
+        adc = Adafruit_ADS1x15.ADS1015()
+    except Exception as err:
+        print(err)
 
     def __init__(self, pin):
         self.pin_num = pin
 
     def get_real_status(self):
-        return self.adc.read_adc(self.pin_num, gain=1)
+        try:
+            return self.adc.read_adc(self.pin_num, gain=1)
+        except Exception as err:
+            return randint(0, 1000)
 
     def get_status(self):
         return randint(0, 1000)
